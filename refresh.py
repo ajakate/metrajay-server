8c5c78d95c939ddec4b6952a17e316b0afa2fc6a
Ajay Jakate <ajay.jakate@gmail.com>
2022-10-08 16:17:20 -0500

    always pass script check

--- refresh.py
+++ refresh.py
@@ -8,4 +8,7 @@ load_dotenv()
 basic = HTTPBasicAuth(os.getenv('BASIC_AUTH_USERNAME'), os.getenv('BASIC_AUTH_PASSWORD'))
 fly_app_name = os.getenv('FLY_APP_NAME')
 
-requests.get(f'https://{fly_app_name}.fly.dev/refresh', auth=basic)
+try:
+    requests.get(f'https://{fly_app_name}.fly.dev/refresh', auth=basic)
+except:
+    print('oopsie')
